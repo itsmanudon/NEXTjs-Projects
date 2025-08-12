d3f450a9bba1817e30c36374592a33dba43a4aa1
Manan Jariwala <mananjariwala01@gmail.com>
2025-08-13 00:41:58 +0530

    Enhance Meals Page Layout and Content
    Updated the Meals page to include a styled header with a welcoming message and a call-to-action link for sharing favorite recipes. Improved overall structure by utilizing CSS modules for better styling.

--- nextjs-complete-guide-course-resources/code/02-nextjs-essentials/Meals-Starting-Porject/app/meals/page.js
+++ nextjs-complete-guide-course-resources/code/02-nextjs-essentials/Meals-Starting-Porject/app/meals/page.js
@@ -1,10 +1,25 @@
-import Link from 'next/link';
+import classes from './page.module.css';
 
 export default function MealsPage() {
     return (
-        <main>
-            <h1>Meals</h1>
-            <p>Here you can find a list of delicious meals!</p>
-        </main>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delecious meals, created{' '}
+                    <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite recipe and cook it yourself. It is easy and fun!
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Share Your Favorite Recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                
+            </main>
+        </>
     );
 }
